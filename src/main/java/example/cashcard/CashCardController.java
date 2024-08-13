e26c010fef2130802cc41005437cac2d663d60a5
justbored11 <justbored1@live.com>
2024-08-13 15:00:42 +0000

    add manual filtering list in findAll cards

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,8 +1,10 @@
 package example.cashcard;
 
 import java.net.URI;
+import java.util.ArrayList;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,7 +46,16 @@ public class CashCardController {
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<CashCard>> findAll() {
-        return ResponseEntity.ok(this.cashCards.findAll());
+    public ResponseEntity<Iterable<CashCard>> findAll(Authentication authentication) {
+
+        var filtered = new ArrayList<CashCard>();
+
+        this.cashCards.findAll().forEach(cashcard->{
+            if (cashcard.owner().equals(authentication.getName())){
+                filtered.add(cashcard);
+            }
+        });
+
+        return ResponseEntity.ok(filtered);
     }
 }
